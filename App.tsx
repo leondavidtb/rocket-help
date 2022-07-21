f6ded6a68eda7421cbca4167aa81bc8f0db41a9a
leondavidtb <leondavidtb@gmail.com>
2022-07-20 21:13:44 -0300

    feat: added home, order and filter components - list with open or closed calls - 'in progress' and 'finished' filters - conditional for empty list

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import { Loading } from "./src/components/Loading";
 
 import { THEME } from "./src/styles/theme";
 import React from "react";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </NativeBaseProvider>
   );
 }
